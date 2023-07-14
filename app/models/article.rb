edc0fa8f42953504ef62d3768401e3fbaf606417
Pablo <musaberpablo@gmail.com>
2023-07-14 13:08:02 -0300

    Validación para titulos y contenido no vacíos.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,13 +3,15 @@ class Article < ApplicationRecord
     belongs_to :user #Campo adicional
     has_many :has_categories
     has_many :categories, through: :has_categories
+    validates :title, presence: true
+    validates :content, presence: true
 
     attr_accessor :category_elements
 
     def save_categories
         return has_categories.destroy_all if category_elements.nil? || category_elements.empty?
 
-        has_categories.where.not(category_id: category_elements).destroy_all 
+        has_categories.where.not(category_id: category_elements).destroy_all
 
         category_elements.each do |category_id|
             HasCategory.find_or_create_by(article: self, category_id: category_id)
