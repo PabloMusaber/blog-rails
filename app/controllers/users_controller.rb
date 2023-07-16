313b15964c48f7f0bd34db7c485a1f7dc1e88079
Pablo <musaberpablo@gmail.com>
2023-07-16 17:55:20 -0300

    Mejoras estéticas generales. Cambios en botones.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
   def edit
   end
 
   def update
     current_user.update(user_params)
-
     redirect_back(fallback_location: root_path)
   end
 
