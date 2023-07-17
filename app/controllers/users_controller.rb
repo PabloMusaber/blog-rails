038ee78ffacb0d2a5d6c1bd2b8c2a56cc95f9877
Pablo <musaberpablo@gmail.com>
2023-07-17 10:59:24 -0300

    Agrega campos al formulario de editar usuario.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :avatar)
   end
 
 end
