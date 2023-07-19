28e43d1eb72813d40965c8f2646f1a06ef57195c
Pablo <musaberpablo@gmail.com>
2023-07-19 09:42:45 -0300

    Mejoras en el formulario de edición de usuario.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :avatar)
+    params.require(:user).permit(:first_name, :last_name, :email, :avatar, :password, :password_confirmation)
   end
 
 end
